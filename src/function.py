a538fd706e47f736c29a5a81cd31da547cd57caf
crlsalejandro <aleocampoiza@gmail.com>
2022-10-11 16:42:42 -0300

    Test first layer

--- src/function.py
+++ src/function.py
@@ -4,5 +4,9 @@ import numpy as np
 def sigmoide(x):
     return 1/1+np.exp(-x)
 
-"""def lineal(x):
-    if x""" 
+def lineal(x):
+    c=9.572340
+    if x>c:
+        return 1
+    else:
+        return 0
