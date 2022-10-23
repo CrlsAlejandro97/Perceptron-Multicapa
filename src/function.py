084564a6843e12dde92edf49301d2ed31d42dd2a
crlsalejandro <aleocampoiza@gmail.com>
2022-10-22 23:01:49 -0300

    calculo de error

--- src/function.py
+++ src/function.py
@@ -2,11 +2,7 @@ from math import exp
 
 import numpy as np
 def sigmoide(x):
-    return 1/1+np.exp(-x)
+    return 1/(1+np.exp(-x))
 
 def lineal(x):
-    c=9.572340
-    if x>c:
-        return 1
-    else:
-        return 0
+    return x
