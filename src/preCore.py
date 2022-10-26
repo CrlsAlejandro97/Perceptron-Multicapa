fe594c3ea6d8060de3cebd74538b6417cd801331
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2022-10-26 18:47:20 -0300

    Representacion de salida con argmax

--- src/preCore.py
+++ src/preCore.py
@@ -5,6 +5,58 @@ import matplotlib.pyplot as plt
 from function import lineal, sigmoide
 import os 
 
+
+"""CONFIGURACION DE LOS DATOS"""
+
+#Leer letras distorsionadas
+
+letras_100 = pd.read_csv("data/distorsionadas/100/letras.csv",sep=';',header=None).to_numpy()
+#letras_500 = pd.read_csv(os.path.join("data","distorsiondas",'100','letras.csv'),sep=';',header=None).to_numpy()
+#letras_1000 = pd.read_csv(os.path.join("data","distorsiondas",'100','letras.csv'),sep=';',header=None).to_numpy()
+
+#Dividir los datos de entrada en validacion, entrenamiento y test, en este caso primero para letras_100
+
+letras_train_porc = 0.8
+letras_test_porc = 0.15
+
+cant_letras_train = int(len(letras_100)*letras_train_porc)
+cant_letras_test = int(len(letras_100)*letras_test_porc)
+
+letras_train = letras_100[:cant_letras_train]
+letras_test = letras_100[cant_letras_train+1:cant_letras_train+cant_letras_test]
+letras_validation = letras_100[cant_letras_train+cant_letras_test+1:]
+
+# El set de validación se utilizará durante iteraciones que haremos con el conjunto de entrenamiento.
+
+
+#Generar los conjuntos de entrenamiento, validacion y test
+
+data_train = []
+for letra in letras_train:
+    x_train = letra[:100]
+    y_train = letra[100:]
+    data_train.append((x_train, y_train))
+
+
+data_test = []
+for letra in letras_test:
+    x_test = letra[:100]
+    y_test = letra[100:]
+    data_train.append((x_test, y_test))
+
+data_validation = []
+for letra in letras_validation:
+    x_validation = letra[:100]
+    y_validation = letra[100:]
+    data_validation.append((x_validation, y_validation))
+
+"""CONFIGURACION DEL PERCEPTRON"""
+
+
+cant_capas_ocultas = 2
+coeficiente_aprendizaje = 0.4
+momento = 0.7
+
 #Cantidades de neuronas por cada capa
 
 cant_ce = 100
@@ -16,17 +68,39 @@ cant_c2 = 5
 
 ce = np.zeros(cant_ce, dtype=int)
 
+
 #Capa de salida
 
 cs = np.zeros(cant_cs, dtype=int)
 
+cs = {}
+for n in range(cant_cs):
+    cs[n] = {
+        "x": 0,
+        "y": 0
+    }
+
 #Primera capa oculta
 
 c1 = np.zeros(cant_c1, dtype=int)
+c1 = {}
+for n in range(cant_c1):
+    c1[n] = {
+        "x": 0,
+        "y": 0
+    }
 
 #Segunda capa oculta
 
 c2 = np.zeros(cant_c2, dtype=int)
+c2 = {}
+for n in range(cant_c2):
+    c2[n] = {
+        "x": 0,
+        "y": 0
+    }
+
+
 
 #Inicializamos los pesos (100 por cada neurona, tenemos 5 neuronas)
 
@@ -83,38 +157,34 @@ W3 = [w31,w32,w33]
 B = random.random()
 
 
-#Leer letras distorsionadas
 
-letras_100 = pd.read_csv("data/distorsionadas/100/letras.csv",sep=';',header=None).to_numpy()
-#letras_500 = pd.read_csv(os.path.join("data","distorsiondas",'100','letras.csv'),sep=';',header=None).to_numpy()
-#letras_1000 = pd.read_csv(os.path.join("data","distorsiondas",'100','letras.csv'),sep=';',header=None).to_numpy()
+for letra in data_train[:1]:
+    x = letra[0]
+    y = letra[1]
+    ce = x[:]
+    for i,n in enumerate(c1.keys()):
+        entrada = np.dot(W1[i],x) + B
+        c1[n]['x'] = entrada
+        c1[n]['y'] = lineal(entrada)
+    
+    for i,n in enumerate(c2.keys()):
+        c2[n]['x'] = np.dot(W2[i],c1[i]['y']) + B
+        c2[n]['y'] = sigmoide(c2[n]['x'])
+    
+    salida = np.empty(3, dtype=float)
+    for i,n in enumerate(cs.keys()):
+        cs[n]['x'] = np.dot(W3[i],c2[i]['y']) + B
+        cs[n]['y'] = sigmoide(cs[n]['x'])
+        salida[i]= (cs[n]['y'])
+
+    s = np.zeros_like(salida, dtype=int)
+    s[salida.argmax(0)] = 1
+    print(s)
 
 
-#Dividir el codigo de la letra de la clase
-cod_clase = []
-for letra in letras_100:
-    codigo = letra[:100]
-    clase = letra[100:]
-    cod_clase.append((codigo, clase))
-    if (clase==y['b']).all():
-        print("Letra b")
-    if (clase==y['d']).all():
-        print("Letra d")
-    if (clase==y['f']).all():
-        print("Letra f")
 
 
 
-#Dividir los datos de entrada en validacion, entrenamiento y test, en este caso primero para letras_100
 
-""" data_train_porc = 0.8
-data_test_porc = 0.15
-data_validation_porc = 0.05
 
-cant_data_train = int(len(letras_100)*data_train_porc)
-cant_data_test = int(len(letras_100)*data_test_porc)
-cant_data_validation = 0
 
-data_train = letras_100[:cant_data_train]
-data_test = letras_100[cant_data_train+1:cant_data_train+cant_data_test]
-data_validation = letras_100[cant_data_train+cant_data_test+1:len()] """
