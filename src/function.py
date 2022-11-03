6e9bbefd48020dd73dc57d5d2ddea3acbecbc32d
crlsalejandro <aleocampoiza@gmail.com>
2022-11-03 17:10:11 -0300

    delta de capas ocultas

--- src/function.py
+++ src/function.py
@@ -11,7 +11,7 @@ def lineal(x):
 def derivate_sigmoide(x):
     return x*(1-x)
 
-def derivate_lineal(x):
+def derivate_lineal():
     return 1 
 
 def derivate_error(ye,ys):
@@ -20,12 +20,16 @@ def derivate_error(ye,ys):
     return -(ye-ys)
 
 def deltaHidden(w,a,deltaInput):
-
+    DELTA = []
     for layer in reversed(w):
         w = np.transpose(layer)
+        delta=[]
         for i in range(len(w)):
-            delta.append(w[i]*deltaInput)
-
+            delta.append(w[i]*deltaInput*derivate_lineal())
+        DELTA.append(delta)
+        deltaInput =np.array(delta)
+    
+    return DELTA
     
 
 
