0e83d6fe0a2101690bd7df1dc8f53964156771be
crlsalejandro <aleocampoiza@gmail.com>
2022-11-04 00:23:02 -0300

    Start Gradient descent

--- src/function.py
+++ src/function.py
@@ -19,6 +19,19 @@ def derivate_error(ye,ys):
     #ys es el valor calculado por la funcion de activacion
     return -(ye-ys)
 
+#------------------------------
+"""def feedFoward(X,W,B,layers):
+    Y=[]
+    #Capas Ocultas
+    for i in range(len(layers)):
+        y=[] #el problema es que la primera es y=X
+        for j in range(len(W)-1):
+            #Suma ponderada
+            Z = np.dot(y,W[j])
+            #Funcion de Activacion
+            y.append(lineal(Z+B[j]))
+        Y.append(np.array(y))"""
+
 def deltaHidden(w,deltaInput):
     #a no se usa porque f.activacion es lineal
     DELTA = []
@@ -44,6 +57,9 @@ def Backpropagation(ye,A,W):
     deltaOut = derivate_error(ye,A[len(A)-1])*derivate_sigmoide(A[len(A)-1])
     #Deltas de salida
     return deltaHidden(W,deltaOut)
+
+
+
     
     
 
