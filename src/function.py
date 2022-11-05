c0c30b24d13611708dd17102137a5c2d6091fddd
crlsalejandro <aleocampoiza@gmail.com>
2022-11-05 14:21:12 -0300

    Listo

--- src/function.py
+++ src/function.py
@@ -19,46 +19,30 @@ def derivate_error(ys,ye):
     #ys es el valor calculado por la funcion de activacion
     return (ys-ye)
 
-#------------------------------
-"""def feedFoward(X,W,B,layers):
-    Y=[]
-    #Capas Ocultas
-    for i in range(len(layers)):
-        y=[] #el problema es que la primera es y=X
-        for j in range(len(W)-1):
-            #Suma ponderada
-            Z = np.dot(y,W[j])
-            #Funcion de Activacion
-            y.append(lineal(Z+B[j]))
-        Y.append(np.array(y))"""
 
-def deltaHidden(W,deltaInput):
-    #a no se usa porque f.activacion es lineal
-    DELTA = []
-    DELTA.append(deltaInput)
-    for layer in reversed(W):
-        wl = np.transpose(layer)
-        delta=[]
-        for i in range(len(wl)):
-            delta.append(np.sum(wl[i]*deltaInput)*derivate_lineal())
-        DELTA.append(delta)
-        deltaInput =np.array(delta)
+# def deltaHidden(W,deltaInput):
+#     #a no se usa porque f.activacion es lineal
+#     DELTA = []
+#     DELTA.append(deltaInput)
+#     for layer in reversed(W):
+#         wl = np.transpose(layer)
+#         delta=[]
+#         for i in range(len(wl)):
+#             delta.append(np.sum(wl[i]*deltaInput)*derivate_lineal())
+#         DELTA.append(delta)
+#         deltaInput =np.array(delta)
     
-    return DELTA
+#     return DELTA
     
 
 
 def calculateDelta(ye,ys3,W):
-    #Ye: es la salida espera
-    #A: vector de salidas de activacion --> se podria eliminar y pasar solo la salida
-    #W: pesos de las capas
+    #ye: salida espera
+    #ys3: salida obtenida
+    #W:  vector peso de las capas
 
     #Delta de capa de salida
     deltaOut = derivate_error(ys3,ye)*derivate_sigmoide(ys3)
-    #Deltas de salida
-    #deltaOut = deltaHidden(W,deltaOut)
-    #Ordenamos delta de inicio a fin
-    #deltaOut = deltaOut[::-1]
     deltas =[]
     deltas.append(deltaOut)
     i=0
@@ -69,7 +53,9 @@ def calculateDelta(ye,ys3,W):
     return deltas[::-1]
 
 def Gradientdescent(deltas,W,Want,activations,B,alfa,beta):
+    #Pesos anteriores
     Wres = []
+    #Pesos actualizados
     Wnext = []
     #Calculo para nuevos pesos
     for i in range(len(W)):
@@ -77,8 +63,11 @@ def Gradientdescent(deltas,W,Want,activations,B,alfa,beta):
         new_weight = []
         #Actualizacion de bias
         B[i] = B[i] - alfa*deltas[i]
+        #Actualizacion de pesos
         for j in range(len(W[i])):
+           #Resguardo peso actual
            waux.append(W[i][j])
+           #Peso actualizado
            new_weight.append(W[i][j] - alfa*deltas[i][j]*activations[i] + beta*(W[i][j] - Want[i][j]))
         waux = np.array(waux)
         new_weight = np.array(new_weight)
